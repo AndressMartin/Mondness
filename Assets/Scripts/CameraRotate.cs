fc94a7a4003b9f43c5c5d221058da76d586c03e2
Kelvin Santos <kelvin.didiei3@gmail.com>
2021-07-10 19:52:11 -0300

    Correção do Giro da Câmera
    Agora a câmera gira corretamente junto com as rotações do personagem e os movimentos apontam para a direção certa.

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -28,25 +28,29 @@ public class CameraRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log("Came Pos: " + posNum + "\nAngulo: " + 90 * posNum);
         if (Input.GetKeyDown(KeyCode.E))
         {
+            transform.Rotate(0, 90, 0);
             posNum++;
         }
         else if (Input.GetKeyDown(KeyCode.Q))
         {
+            transform.Rotate(0, -90, 0);
             posNum--;
         }
         else return;
         if (posNum > 3) posNum = 0;
         if (posNum < 0) posNum = 3;
         y = 90 * posNum;
-        SetCameraNewPos();
+        cameraPos = (Enums.CameraPos)posNum;
+        //SetCameraNewPos();
     }
 
     private void SetCameraNewPos()
     {
         cameraPos = (Enums.CameraPos)posNum;
-        currentEulerAngles = new Vector3(x, y, z);
+        currentEulerAngles = new Vector3(transform.rotation.x, y, transform.rotation.z);
         //TODO: Fazer IENUMERATOR com animação da câmera
         Debug.Log($"currentEulerAngles = {currentEulerAngles}");
         currentRotation.eulerAngles = currentEulerAngles;
